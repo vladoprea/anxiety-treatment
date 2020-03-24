6042f711519f3cb0792614dd7fe3470586fdd370
vladoprea <opreavladteofil@gmail.com>
2020-03-24 13:35:35 +0000

    Render home.html with navbar

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from flask import Flask, render_template, redirect, request, url_for
 import os
-from flask import Flask
- 
+from os import path
+if path.exists("env.py"):
+    import env
+
+
 app = Flask(__name__)
- 
- 
+
+app.secret_key = os.environ.get("SECRET_KEY")
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+mongo = PyMongo(app)
+
+
 @app.route('/')
-def hello():
-    return 'Hello'
- 
- 
+def home():
+    return render_template('home.html')
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get('PORT')),
