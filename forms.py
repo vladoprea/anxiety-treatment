1131c610209b722cb2d8ed1b771ac4f4613aa54e
Vlad Oprea <opreavladteofil@gmail.com>
2020-05-06 13:30:33 +0100

    Change validators syntax

--- forms.py
+++ forms.py
@@ -1,12 +1,13 @@
 import os
-from wtforms import Form, StringField, TextAreaField, PasswordField, SelectField, validators
-from wtforms.validators import DataRequired, EqualTo
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, PasswordField, SelectField, validators
+from wtforms.validators import DataRequired, EqualTo, Length
 
 
 #Register Form Class
-class RegisterForm(Form):
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    email = StringField('Email', [validators.Length(min=6, max=50)])
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators = [Length(min=4, max=25)])
+    email = StringField('Email', validators = [Length(min=6, max=50)])
     password = PasswordField('Password', [
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords must match')
@@ -14,18 +15,18 @@ class RegisterForm(Form):
     confirm = PasswordField('Confirm Password')
 
 #Login Form Class
-class LoginForm(Form):
-    username = StringField('Username', [validators.Length(min=4, max=25)])
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators = [Length(min=6, max=50)])
     password = PasswordField('Password', validators=[DataRequired()])
 
 #Journal Entry Class
-class JournalForm(Form):
-    title = StringField('Title', [validators.Length(min=10, max=100)])
+class JournalForm(FlaskForm):
+    title = StringField('Title', validators = [Length(min=4, max=25)])
     body = TextAreaField('Description')
 
 #TFB Cycle Entry Class
-class ToughtsForm(Form):
-    situation = StringField('Situation', [validators.Length(min=3)])
+class ToughtsForm(FlaskForm):
+    situation = StringField('Situation', validators = [Length(min=4, max=25)])
     feeling = StringField('Feeling')
     rate_feeling = SelectField(u'Rate Feeling', choices = [('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10')])
     physical = StringField('Physical Reaction')
